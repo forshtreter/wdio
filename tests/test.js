ef45501e0a0abf27c720d6d3359d4a2974517616
Anton Vdovichenko <ant-vav@yandex-team.ru>
2017-07-20 15:15:52 +0500

    New good test

--- tests/test.js
+++ tests/test.js
@@ -5,11 +5,16 @@ describe('Test suit', function () {
     it('test to fail', function () {
         return browser.url('http://webdriver.io')
             .then(() => browser.element('header .header').waitForVisible());
-    })
+    });
 
     it('test to pass', function () {
         return browser.url('http://webdriver.io')
             .then(() => browser.element('header .header').isVisible())
             .then(result => assert(result));
-    })
+    });
+
+    it('test to fail 2', function () {
+        return browser.url('http://webdriver.io')
+            .then(() => browser.waitForVisible('header .header'));
+    });
 });
